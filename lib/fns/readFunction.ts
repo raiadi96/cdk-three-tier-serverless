dc98e6036da1f95730f054cc929dcdc1001ef945
Aditya Rai <kaditya1298@gmail.com>
2023-04-22 23:00:43 +0530

    Lambda-CDK

---
+++ lib/fns/readFunction.ts
@@ -0,0 +1,7 @@
+import {APIGatewayProxyResultV2} from 'aws-lambda';
+import {Notes} from './notesTable';
+
+export const handler = async (): Promise<APIGatewayProxyResultV2> =>{
+    const notes =  await Notes.find({pk:'note'}, {limit :10, reverse:true});
+    return {body : JSON.stringify(notes), statusCode:200}
+}
